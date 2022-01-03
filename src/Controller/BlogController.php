130f3df760e485a5e82d279a088ce75dfb7d869f
GrosTomi <95704903+GrosTomi@users.noreply.github.com>
2022-01-03 09:06:19 +0100

    Update BlogController.php

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,20 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
-     */
-    public function index(): Response
-    {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
-    }
-
-    /**
      * @Route("/main", name="main")
      */
     public function main()
     {
-        return $this->render('main.html.twig'); 
+        return $this->render('sheet/main.html.twig'); 
     }
 }
