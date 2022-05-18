69b4b8875ccd4939ecfbeb0306929162fa763c02
GrosTomi <95704903+GrosTomi@users.noreply.github.com>
2022-05-18 16:31:19 +0200

    Update wordpress_saniossian.ps1

--- scripts et docker/wordpress_saniossian.ps1
+++ scripts et docker/wordpress_saniossian.ps1
@@ -1,3 +1,17 @@
 docker pull mariadb
 docker pull wordpress
-docker run --detach --name mariadbConteneur --env MARIADB_USER=root --env MARIADB_PASSWORD=root --env MARIADB_ROOT_PASSWORD=root  --env MARIADB_DATABASE=mydb -p 3306:3306 mariadb:latest
+docker run --detach --name mariadbConteneur --env MARIADB_USER=user --env MARIADB_PASSWORD=paswd --env MARIADB_ROOT_PASSWORD=root  --env MARIADB_DATABASE=mydb -p 3306:3306 mariadb:latest
+docker exec -it conteneur_mariadb bash
+ip a
+apt-get update
+apt-get install iputils-ping
+ping mariadbConteneur
+exit
+docker network create sae23_saniossian
+docker run --detach --name mariadbConteneur --env MARIADB_USER=user --env MARIADB_PASSWORD=paswd --env MARIADB_ROOT_PASSWORD=root  --env MARIADB_DATABASE=mydb -p 3306:3306 --network sae23_saniossian mariadb:latest
+docker exec -it mariadbConteneur bash
+ip a
+apt-get update
+apt-get install iputils-ping
+ping mariadbConteneur
+exit
